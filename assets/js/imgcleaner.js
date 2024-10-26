7081a6c0c70d00b0b4991a722e8eb724ca2c7ced
MoxiDough <moxiboxidoo@proton.me>
2024-10-26 18:01:52 +0200

    Add files via upload

--- assets/js/imgcleaner.js
+++ assets/js/imgcleaner.js
@@ -1,6 +1,21 @@
-document.getElementById("clear-button").addEventListener('click', function test() {
+document.getElementById("clear-button").addEventListener('click', function bike() {
+  document.getElementById("clear-button-warning-box").style.display= "flex";
+  document.getElementById("clear-button-warning-bg").style.display= "flex";
+
+});
+
+document.getElementById("clear-button-warning-bg").addEventListener('click', function mike() {
+  document.getElementById("clear-button-warning-box").style.display= "none";
+  document.getElementById("clear-button-warning-bg").style.display= "none";
+  document.getElementById('settings-content').style.marginTop  = "-5.5em";
+  document.getElementById('settings-content').style.minHeight = "80%"
+});
+
+document.getElementById("clear-button-warning-btn").addEventListener('click', function dike() {
   document.querySelector("#content-images").replaceChildren();
   document.getElementById("upload-box").style.display= "flex";
+  document.getElementById("clear-button-warning-box").style.display= "none";
+  document.getElementById("clear-button-warning-bg").style.display= "none";
   document.getElementById('settings-content').style.marginTop  = "-5.5em";
   document.getElementById('settings-content').style.minHeight = "80%"
 
